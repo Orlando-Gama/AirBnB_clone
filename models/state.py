b8eca9c748484e923315c545726c78c32f704607
kantenguleashan <kantenguleshan@gmail.com>
2023-10-27 13:26:44 -0700

    new files

---
+++ models/state.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""DEFINES THE STATE CLASS."""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """REP A STATE CLASS.
+
+    Attributes:
+        name (str): The name of the state.
+    """
+
+    name = ""
